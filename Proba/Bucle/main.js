603e6d8258fe1c8576f5f82b4a3c54c5660aa2c8
R4bits <R4bbits@pm.me>
2023-10-02 13:44:40 +0200

    ejercicio bucle

--- Proba/Bucle/main.js
+++ Proba/Bucle/main.js
@@ -1,2 +1,24 @@
+const fruteria = ['🍎', '🍐', '🍌', '🍇']
 
-const frase = Con cien cañones por banda...
+for (let i = 0; i < fruteria.length; i++) {
+    const fruta = fruteria[i]
+    console.log(fruta)
+}
+
+debugger
+
+const numeros = [2, 15, -13, 23, 75, 4, 12];
+const numerosimpares = []
+
+for (let i = 0; i < numeros.length; i++) {
+    let numero = numeros[i]
+
+    if (numero % 2 !== 0) {
+        numero += 5
+        numerosimpares.push(numero)
+    }
+}
+
+console.log(numerosimpares)
+
+
