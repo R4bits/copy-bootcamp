0b9aaaf6f858115917d9066db299ab653132b7f8
R4bits <R4bbits@pm.me>
2023-10-02 17:14:18 +0200

    All loops complete 100%

--- Proba/Bucle/main.js
+++ Proba/Bucle/main.js
@@ -1,24 +1,16 @@
 const fruteria = ['🍎', '🍐', '🍌', '🍇']
 
 for (let i = 0; i < fruteria.length; i++) {
-    const fruta = fruteria[i]
-    console.log(fruta)
+    console.log(`Fruta ${i + 1}: ${fruteria[i]}`)
 }
 
-debugger
+const frutasIndiceImpar = []
 
-const numeros = [2, 15, -13, 23, 75, 4, 12];
-const numerosimpares = []
+for (let i = 0; i < fruteria.length; i++) {
 
-for (let i = 0; i < numeros.length; i++) {
-    let numero = numeros[i]
-
-    if (numero % 2 !== 0) {
-        numero += 5
-        numerosimpares.push(numero)
+    if (i % 2 === 1) {
+        frutasIndiceImpar.push(fruteria[i])
     }
 }
 
-console.log(numerosimpares)
-
-
+console.log(frutasIndiceImpar)
