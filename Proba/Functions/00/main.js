8d67782a61507c871f9a43abd14ca6355e4def50
R4bits <R4bbits@pm.me>
2023-10-03 13:51:04 +0200

    Functions

---
+++ Proba/Functions/00/main.js
@@ -0,0 +1,24 @@
+
+function compa(num1, num2) {
+    if (num1 === num2) {
+        console.log('Son iguales')
+
+    } else if (num1 > num2) {
+        console.log(num1 + ', es mayor que ' + num2)
+
+    } else console.log(num2 + ', es mayor que ' + num1)
+}
+
+// compa(4, 8)
+
+
+function bool(num1, num2) {
+    if (num1 === num2) {
+        console.log('True')
+
+    } else console.log('False')
+}
+
+// bool(8, 4)
+
+function 
